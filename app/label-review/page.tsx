1f15d63b3ca130ae19522509ad207e22e7ce4058
Ha Minh Quoc Bao <120712594+baoha7204@users.noreply.github.com>
2026-09-10 10:33:01 +0700

    fix: remove AppShell due to refactor to shared layout

--- app/label-review/page.tsx
+++ app/label-review/page.tsx
@@ -1,10 +1,5 @@
-import { AppShell } from "@/components/AppShell";
 import { LabelReviewClient } from "@/components/LabelReviewClient";
 
 export default function LabelReviewPage() {
-  return (
-    <AppShell>
-      <LabelReviewClient />
-    </AppShell>
-  );
+  return <LabelReviewClient />;
 }
